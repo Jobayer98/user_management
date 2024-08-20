4892df56ae4d5fb763add418eb4eec0a291c60f0
Jobayer98 <joobayerrahman0220@gmail.com>
2024-08-20 12:35:37 +0600

    .

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RegistrationView
 
 urlpatterns = [
-    path('sign-up', RegistrationView.as_view(), name='token_obtain_pair'),
+    path('sign-up', RegistrationView.as_view(), name='register user'),
 ]
